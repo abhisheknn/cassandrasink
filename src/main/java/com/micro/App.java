d222bae66bee3aacbc8b7326d543e7bcb1febc3a
Nagpure <Abhishek_Nagpure@McAfee.com>
2019-03-08 16:00:46 +0530

    add create-table consumer

---
+++ src/main/java/com/micro/App.java
@@ -0,0 +1,17 @@
+package com.micro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App {
+
+	@Autowired
+	CassandraTable cassandraTable;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
